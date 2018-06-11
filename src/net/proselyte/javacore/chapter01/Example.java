b8a6fc9bb164f40e92844278cd1814f4f24e9cf7
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-06-11 15:07:31 +0300

    first study programs

---
+++ src/net/proselyte/javacore/chapter01/Example.java
@@ -0,0 +1,13 @@
+package net.proselyte.javacore.chapter01;
+
+/*
+Первая программа на Java, "Hello World!"
+ */
+
+public class Example {
+    //psvm --> авт. создание функции main
+    public static void main(String args[]){
+        //Вывод в командную строку: Hello World!
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/net/proselyte/javacore/chapter01/Example2.java
@@ -0,0 +1,14 @@
+package net.proselyte.javacore.chapter01;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int num; //объявление переменной
+        num = 100;
+
+        System.out.println("Переменная num равна: " + num);
+
+        num = num*2; //Изменине num, теперь она равна будет 200
+
+        System.out.println("Новая num: " + num);
+    }
+}
