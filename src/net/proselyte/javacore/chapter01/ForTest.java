e7f8cb64b7e117dbbceddb7b85155afb85dc0e29
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-06-12 12:20:26 +0300

    Adding file with 'for'

---
+++ src/net/proselyte/javacore/chapter01/ForTest.java
@@ -0,0 +1,11 @@
+package net.proselyte.javacore.chapter01;
+
+public class ForTest {
+    public static void main(String[] args) {
+        int x;
+
+        for(x = 0; x < 10; x++)
+            System.out.println("x равен: " + x);
+        // x++ тоже самое , что x = x +1
+    }
+}
