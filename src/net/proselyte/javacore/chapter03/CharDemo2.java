b85095f9cbb161b931c2de02ec0520c7d4eadf15
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-06-12 23:07:35 +0300

    Test char ch1++ X-->Y

---
+++ src/net/proselyte/javacore/chapter03/CharDemo2.java
@@ -0,0 +1,10 @@
+package net.proselyte.javacore.chapter03;
+
+public class CharDemo2 {
+    public static void main(String[] args) {
+        //символьные переменные ведут себя также как и целочисленные
+        char ch1 = 'X';
+        ch1++;
+        System.out.println("ch1 теперь: " + ch1); // будет Y
+    }
+}
