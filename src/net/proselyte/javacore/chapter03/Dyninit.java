e0ca973d80fb18d2f805c66004507ee03f55ac6c
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-06-17 15:50:14 +0300

    Dynamic init

---
+++ src/net/proselyte/javacore/chapter03/Dyninit.java
@@ -0,0 +1,13 @@
+package net.proselyte.javacore.chapter03;
+public class Dyninit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a = 3.0, b = 4.0;
+		
+		//Динамическая инициализация переменной гипотинузы
+		double c = Math.sqrt(a*a + b*b);
+		System.out.println("Гипотенуза равна: " + c);
+	}
+
+}
