0fecf25aeec7b369df1e97bf502989add4258b4e
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-06-17 17:57:45 +0300

    Ternary operation

--- src/net/proselyte/javacore/chapter04/Ternary.java
+++ src/net/proselyte/javacore/chapter04/Ternary.java
@@ -4,15 +4,14 @@ public class Ternary {
     public static void main(String[] args) {
         int i, k;
 
-        i = -15;
-        k = i < 0 ? -i : i; //
-        System.out.print("Абсолютное значение: ");
-        System.out.println(i + " равно " + k);
-
         i = 15;
-        k = i < 0 ? -i : i; //
+        k = i < 0 ? -i : i;
         System.out.print("Абсолютное значение: ");
         System.out.println(i + " равно " + k);
 
+        i = -15;
+        k = i < 0 ? -i : i;
+        System.out.print("Абсолютное значение: ");
+        System.out.println(i + " равно " + k);
     }
 }
