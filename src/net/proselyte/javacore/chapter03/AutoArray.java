a61cf2376670f93d64b202a34a0bc977fa466432
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-06-17 15:45:59 +0300

    Another example with array

---
+++ src/net/proselyte/javacore/chapter03/AutoArray.java
@@ -0,0 +1,12 @@
+package example;
+// Улучшенная версия массива
+public class AutoArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int month_days[] = {30, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 31};
+		
+		System.out.println("В апреле " + month_days[3] + " дней");
+	}
+
+}
