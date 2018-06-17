61f3c484ec512289dacc2ca5fafc7f1443901951
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-06-17 18:17:09 +0300

    elif example

---
+++ src/net/proselyte/javacore/chapter05/IfElse.java
@@ -0,0 +1,21 @@
+package net.proselyte.javacore.chapter05;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int month = 4;
+        String season;
+
+        if(month==12 || month==1 || month==2)
+            season = "Зиме";
+        else if(month==3 || month==4 || month==5)
+            season = "Весне";
+        else if(month==6 || month==7 || month==8)
+            season = "Лету";
+        else if(month==9 || month==10 || month==11)
+            season = "Осени";
+        else
+            season = "к вымышленным месяцам";
+
+        System.out.println("Апрель относится к " + season);
+    }
+}
