e9254de9761cfb973b0c51a496e91434707ffb82
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-06-22 10:05:29 +0300

    init in for

---
+++ src/net/proselyte/javacore/chapter05/ForTick.java
@@ -0,0 +1,10 @@
+package net.proselyte.javacore.chapter05;
+
+public class ForTick {
+    public static void main(String[] args) {
+        int n;
+
+        for(n=10; n>0; n--)
+            System.out.println("Такт " + n);
+    }
+}
