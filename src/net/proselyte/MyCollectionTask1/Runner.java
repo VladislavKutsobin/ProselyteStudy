b4c32f708d0c2537780f6232e3fe01ff95ced190
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-07-17 16:00:18 +0300

    remake

--- src/net/proselyte/MyCollectionTask1/Runner.java
+++ src/net/proselyte/MyCollectionTask1/Runner.java
@@ -4,16 +4,18 @@ public class Runner {
     public static void main(String[] args) {
         MyCollection mc = new MyCollection();
         //input elements
+        mc.add(6);
+        mc.add(9);
         mc.add(5);
-        mc.add(4);
-        mc.add(7);
+        mc.add(14);
+        mc.GetArray();
         System.out.println("Элемент по индексу: " + mc.GetValue(1));
         System.out.println("Элемент по значению и его индекс: " + mc.GetIndex(11));
         mc.GetArray();
         System.out.println("Минимальный элемент: " + mc.MinValue());
         System.out.println("Максимальный элемент: " + mc.MaxValue());
         System.out.println("Среднее по массиву: " + mc.GetAverage());
-        System.out.println("Удаление элемента: " + mc.Remove(11));
+        System.out.println("Удаление элемента: " + mc.Remove(14));
         mc.GetArray();
     }
 }
