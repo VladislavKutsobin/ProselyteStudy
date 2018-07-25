9b193b37faf654d82e244e0ee8ba54175e457dce
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-07-25 19:12:57 +0300

    Error correction

--- src/net/proselyte/MyCollectionTask1/README.md
+++ src/net/proselyte/MyCollectionTask1/README.md
@@ -4,17 +4,19 @@ Example Program Version 1.0 19.07.2018
 ## GENERAL USAGE NOTES
 
 This Program has two java-files. Runner.java is main. You can test all capabilities
-from MyCollection.java.
-MyCollection.java implements int type object collection. This collection has functions
+from ChangebleArray.java.
+ChangebleArray.java implements int type object collection. This collection has functions
 such as:
-* GetValue(): > Searching element by index.
-* GetIndex(): > Searching index by value.
+* getValue(): > Searching element by index.
+* getIndex(): > Searching index by value.
 * add(): > Adding element to end of list, and changing all previous element like that: arr[i] = arr[i] + integer
 * remove(): > Removing element by index and changing all other element like that : arr[i] = arr[i] - integer
-* MinValue(): > Searching min value in list
-* MaxValue(): > Searching max value in list
-* GetAverage(): > Return average of list
-* GetArray(): > Return array
+* minValue(): > Searching min value in list
+* maxValue(): > Searching max value in list
+* getAverage(): > Return average of list
+* getArray(): > Return array
+
+How we can see, this collection allows to change other elements in array, when elements depend from new element
 
 ---
 
--- src/net/proselyte/MyCollectionTask1/Runner.java
+++ src/net/proselyte/MyCollectionTask1/Runner.java
@@ -2,21 +2,21 @@ package net.proselyte.MyCollectionTask1;
 
 public class Runner {
     public static void main(String[] args) {
-        MyCollection mc = new MyCollection();
+        ChangebleArray mc = new ChangebleArray();
         //input elements
         mc.add(6);
         mc.add(9);
         mc.add(5);
         mc.add(14);
-        mc.GetArray();
-        System.out.println("Элемент по индексу: " + mc.GetValue(1));
-        System.out.println("Элемент по значению и его индекс: " + mc.GetIndex(11));
-        mc.GetArray();
-        System.out.println("Минимальный элемент: " + mc.MinValue());
-        System.out.println("Максимальный элемент: " + mc.MaxValue());
-        System.out.println("Среднее по массиву: " + mc.GetAverage());
-        System.out.println("Удаление элемента: " + mc.Remove(14));
-        mc.GetArray();
+        mc.getArray();
+        System.out.println("Элемент по индексу: " + mc.getValue(1));
+        System.out.println("Элемент по значению и его индекс: " + mc.getIndex(11));
+        mc.getArray();
+        System.out.println("Минимальный элемент: " + mc.minValue());
+        System.out.println("Максимальный элемент: " + mc.maxValue());
+        System.out.println("Среднее по массиву: " + mc.getAverage());
+        System.out.println("Удаление элемента: " + mc.remove(14));
+        mc.getArray();
     }
 }
 
