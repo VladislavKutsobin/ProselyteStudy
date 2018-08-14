efdb12a9cef2c71198609a1549f31360a705a9c6
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-08-14 17:46:40 +0300

    Simple applet

---
+++ src/net/proselyte/javacore/chapter13/SimpleApplet.java
@@ -0,0 +1,13 @@
+package net.proselyte.javacore.chapter13;
+import java.awt.*;
+import java.applet.Applet;
+/*
+<applet code="SimpleApplet" width=200 height=60>
+</applet>
+ */
+
+public class SimpleApplet extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("Simple applet", 20, 20);
+    }
+}
