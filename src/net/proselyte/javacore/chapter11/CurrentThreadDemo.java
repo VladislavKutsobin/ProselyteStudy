ae01b0b17b97ca5ebf83c491d37487c6ba6d01ea
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-09-29 23:47:41 +0300

    Threads demo

---
+++ src/net/proselyte/javacore/chapter11/CurrentThreadDemo.java
@@ -0,0 +1,20 @@
+package net.proselyte.javacore.chapter11;
+
+public class CurrentThreadDemo {
+    public static void main(String[] args) {
+        Thread t = Thread.currentThread();
+
+        System.out.println("Текущий поток віполнения: " + t);
+        t.setName("My Thread");
+        System.out.println("Имя потока после изменения: " + t);
+
+        try {
+            for(int n=5; n>0; n--) {
+                System.out.println(n);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Главный поток прерван");
+        }
+    }
+}
